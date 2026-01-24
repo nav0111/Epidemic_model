2e66c5024fcc1990526899770ade35aa543c5477
nav0111 <nav011@uregina.ca>
2026-01-24 16:02:45 -0600

    just stop showing plot

--- RK_solver.py
+++ RK_solver.py
@@ -3,13 +3,6 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import torch
 
-
-
-#plt.plot(simulationTime, betaArray)
-#plt.xlabel("Time")
-#plt.ylabel("Transmission rate beta")
-#plt.show()
-
 def sigmoid(x, a, b):
     x = np.asarray(x)
     return 1 / (1 + np.exp(-a*(x-b)))
@@ -22,7 +15,7 @@ def create_beta_array(time_domain):
     beta_array = beta_min + (beta_max - beta_min) * sigmoid(time_domain, a, b)
     return beta_array
 
-def seirStateSpaceModel(y,t,timePoints, sigma0, gamma0, betaArray, N):
+"""def seirStateSpaceModel(y,t,timePoints, sigma0, gamma0, betaArray, N):
     S, E, I =y
     beta_t = np.interp(t, timePoints, betaArray)
     print(beta_t)
@@ -30,14 +23,31 @@ def seirStateSpaceModel(y,t,timePoints, sigma0, gamma0, betaArray, N):
     dSdt = -beta_t *S*I /N
     dEdt = beta_t *S *I /N - sigma0*E
     dIdt = sigma0*E - gamma0*I
-    return [dSdt, dEdt, dIdt]
+    return [dSdt, dEdt, dIdt]"""
 
-def run_seir_model(): 
+#SEIHRD model
+def seirhrd_model(y,t,time_points, sigma0, gamma_u0, gamma_r0,p0,gamma_h0,mu0, beta_array, N):
+    S, E, I_u, I_r, H, R, D =y
+    beta_t = np.interp(t,time_points, beta_array)
+    dSdt = -beta_t * S * (I_u + I_r) /N
+    dEdt = beta_t * S * (I_u + I_r) /N - sigma0*E
+    dI_udt = p0 * sigma0 * E - gamma_r0 * I_r
+    dI_rdt = (1-p0) * sigma0 * E - gamma_h0 * I_r
+    dHdt = gamma_h0 * I_r - gamma_u0 * H
+    dRdt = gamma_u0 * H + gamma_r0 * I_r
+    dDdt = mu0 * H
+    return [dSdt, dEdt, dI_udt, dI_rdt, dHdt, dRdt, dDdt]
+
+def run_seihrd_model(): 
     # define parameters
     N= 1000
     sigma0 = 1/5.2
-    gamma0 = 1/7  #recovery rate
-    startTime = 0
+    gamma_u0 = 1/10
+    gamma_r0 = 1/7
+    p0 = 0.8
+    gamma_h0 = 1/14
+    mu0 = 0.01
+    startTime =0
     endTime = 100
     timeSteps = 1000
     simulationTime = np.linspace(startTime, endTime, timeSteps)
@@ -45,27 +55,38 @@ def run_seir_model():
     beta_array = create_beta_array(simulationTime)
     
     # define initial conditions
-    S0, E0, I0 = 990, 7, 3 
-    
+    S0, E0, I_u0, I_r0, H0, R0, D0 = 990,5, 2,3, 0,0,0
+     
     # run simulation
-    initialState = np.array([S0, E0, I0])
-    solutionState = odeint(seirStateSpaceModel, initialState, simulationTime, args=(simulationTime, sigma0, gamma0, beta_array, N))
-    
+    initialState = np.array([S0, E0, I_u0, I_r0, H0, R0, D0])
+    solutionState = odeint(seirhrd_model, initialState, simulationTime, args=(simulationTime, sigma0, gamma_u0, gamma_r0,p0,gamma_h0,mu0,beta_array,N))
     # plot results
     plt.plot(simulationTime, solutionState[:,0], label='S', color = 'blue')
     plt.plot(simulationTime, solutionState[:,1], label='E', color = 'red')
-    plt.plot(simulationTime, solutionState[:,2], label='I', color = 'green')
+    plt.plot(simulationTime, solutionState[:,2], label='I_u', color = 'green')
+    plt.plot(simulationTime, solutionState[:,3], label ='I_r', color = 'orange')
+    plt.plot(simulationTime, solutionState[:,4], label='H', color = 'purple')
+    plt.plot(simulationTime, solutionState[:,5], label= 'R', color = 'brown')
+    plt.plot(simulationTime, solutionState[:,6], label= 'D', color = 'black')
     plt.xlabel("time")
     plt.ylabel('population')
-    plt.show()
+    plt.legend()
+    plt.close()
 
     return solutionState
 
 
 
-for (i, val) in enumerate(nn):
+"""for (i, val) in enumerate(nn):
     if isinstance(val, torch.nn.Linear):
         print(f"layer number: {i}, in: {val.in_features}, out: {val.out_features}")
-        print(f"weights: {val.weight}")
+        print(f"weights: {val.weight}")"""
+
+
+def add_noise(data, noise_level):
+    noise = np.random.normal(0, noise_level, data.shape)
+    return data + noise
+
+
 
 
