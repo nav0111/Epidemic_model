f148ed3e594ea356997c59edc613cf3c86090888
Affan S <4442679+affans@users.noreply.github.com>
2026-01-28 15:59:07 -0600

    minor cleanup of RK solver

--- RK_solver.py
+++ RK_solver.py
@@ -3,6 +3,12 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 import torch
 
+# in ipython, run the following for interactive reloading
+# %load_ext autoreload
+# %autoreload 2
+# import RK_solver
+
+
 def sigmoid(x, a, b):
     x = np.asarray(x)
     return 1 / (1 + np.exp(-a*(x-b)))
@@ -14,6 +20,10 @@ def create_beta_array(time_domain):
     beta_max = 1.1
     beta_array = beta_min + (beta_max - beta_min) * sigmoid(time_domain, a, b)
     return beta_array
+
+def add_noise(data, noise_level):
+    noise = np.random.normal(0, noise_level, data.shape)
+    return data + noise
 
 """def seirStateSpaceModel(y,t,timePoints, sigma0, gamma0, betaArray, N):
     S, E, I =y
@@ -60,33 +70,26 @@ def run_seihrd_model():
     # run simulation
     initialState = np.array([S0, E0, I_u0, I_r0, H0, R0, D0])
     solutionState = odeint(seirhrd_model, initialState, simulationTime, args=(simulationTime, sigma0, gamma_u0, gamma_r0,p0,gamma_h0,mu0,beta_array,N))
-    # plot results
-    plt.plot(simulationTime, solutionState[:,0], label='S', color = 'blue')
-    plt.plot(simulationTime, solutionState[:,1], label='E', color = 'red')
-    plt.plot(simulationTime, solutionState[:,2], label='I_u', color = 'green')
-    plt.plot(simulationTime, solutionState[:,3], label ='I_r', color = 'orange')
-    plt.plot(simulationTime, solutionState[:,4], label='H', color = 'purple')
-    plt.plot(simulationTime, solutionState[:,5], label= 'R', color = 'brown')
-    plt.plot(simulationTime, solutionState[:,6], label= 'D', color = 'black')
-    plt.xlabel("time")
-    plt.ylabel('population')
-    plt.legend()
-    plt.close()
 
     return solutionState
 
-
-
-"""for (i, val) in enumerate(nn):
-    if isinstance(val, torch.nn.Linear):
-        print(f"layer number: {i}, in: {val.in_features}, out: {val.out_features}")
-        print(f"weights: {val.weight}")"""
-
-
-def add_noise(data, noise_level):
-    noise = np.random.normal(0, noise_level, data.shape)
-    return data + noise
-
+def plot_solution(solutionState):
+    # if time start and end change, 
+    # update here and in run_seihrd_model.
+    ts = np.linspace(0, 100, solutionState.shape[0])
+    # plot results
+    plt.plot(ts, solutionState[:,0], label='S', color = 'blue')
+    plt.plot(ts, solutionState[:,1], label='E', color = 'red')
+    plt.plot(ts, solutionState[:,2], label='I_u', color = 'green')
+    plt.plot(ts, solutionState[:,3], label ='I_r', color = 'orange')
+    plt.plot(ts, solutionState[:,4], label='H', color = 'purple')
+    plt.plot(ts, solutionState[:,5], label= 'R', color = 'brown')
+    plt.plot(ts, solutionState[:,6], label= 'D', color = 'black')
+    plt.xlabel("time")
+    plt.ylabel('population')
+    plt.legend()
+    plt.show()
+    plt.close()
 
 
 
